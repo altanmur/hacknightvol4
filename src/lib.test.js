f680e790af5e00258854608810c0482fa7e4df0c
Altanmur Tumurbat <altanmor@gmail.com>
2019-02-28 20:40:56 +0800

    [change] test to correct

--- src/lib.test.js
+++ src/lib.test.js
@@ -1,5 +1,5 @@
 const lib = require('./lib');
 
 test('helloWorld() returns some special text', () => {
-  expect(lib.helloWorld()).toBe('Hello World1');
+  expect(lib.helloWorld()).toBe('Hello World');
 });
